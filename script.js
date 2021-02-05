87728a51f5524180989c5075a2d18ccc4ab0dcd2
Corbin Garner <corbingar@gmail.com>
2021-02-04 20:23:07 -0800

    modified:   SearchStore.js 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 //event listener for both search boxes
 $("#search-btn").click(function(){
-    getFiveDay()
-    console.log("it works i promise")
+
+    
+
+
+
+
+    var cty1 = document.getElementById( "searchCityOne" );
+    var cty2 = document.getElementById( "searchCityTwo" );
+
+    var cty11 = cty1.value;
+    var cty22 = cty2.value;
+
+
+    console.log(cty11)
+    console.log(cty22)
+
+    StoreItem("CityOne", cty11)
+    StoreItem("CityTwo", cty22)
+    StoreItem("Citythree", cty11)
+
+
+
+    LoadItem("CityOne")
+    LoadItem("CityTwo")
+
+    console.log("loaded 1 " +LoadItem("CityOne"))
+    console.log("loaded 2 " +LoadItem("Citytwo"))
+    console.log("loaded 2 " +LoadItem("Citythree"))
 })
+
