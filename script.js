d48dedc784d800cac54ccdff311786a59aa9a355
Mike Coletta <coletta.mike@gmail.com>
2021-02-06 10:50:08 -0800

    Local storage event listener added

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //event listener for both search boxes
 $("#search-btn").click(function(){
+    StoreItem("cityOne", $("#searchCityOne").val())
+    StoreItem("cityTwo", $("#searchCityTwo").val())
+    LoadItem("cityOne")
     getFiveDay()
+    
     console.log("it works i promise")
 })
